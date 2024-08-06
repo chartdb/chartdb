05e1f142086f6e3847ba61393b77ad6c546f963f
Guy Ben-Aharon <baguy3@gmail.com>
2024-08-06 18:20:33 +0300

    side bar menu

--- src/pages/editor-page/side-panel/side-panel.tsx
+++ src/pages/editor-page/side-panel/side-panel.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from '@/components/select/select';
 
 export interface SidePanelProps {}
 
 export const SidePanel: React.FC<SidePanelProps> = () => {
-    return <aside className="flex h-full"></aside>;
+    return (
+        <aside className="flex h-full flex-col">
+            <div className="flex justify-center border-b">
+                <Select defaultValue="tables">
+                    <SelectTrigger className="mt-1 border-none shadow-none focus:border-transparent focus:ring-0">
+                        <SelectValue />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectItem value="tables">Tables</SelectItem>
+                            <SelectItem value="relationships">
+                                Relationships
+                            </SelectItem>
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+            </div>
+        </aside>
+    );
 };
