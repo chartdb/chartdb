3b2a7f8a1fa5a517576638b1b526f9e9e298c570
Guy Ben-Aharon <baguy3@gmail.com>
2024-08-25 18:34:14 +0300

    add husky

--- src/dialogs/create-diagram-dialog/create-diagram-dialog.tsx
+++ src/dialogs/create-diagram-dialog/create-diagram-dialog.tsx
@@ -151,7 +151,7 @@ export const CreateDiagramDialog: React.FC<CreateDiagramDialogProps> = ({
     }, []);
 
     const renderExamplesOption = useCallback(
-        () =>(
+        () => (
             <div
                 className="flex w-32 h-32 bg-pink-50 rounded-md items-center text-center flex-col py-3 border border-pink-600 cursor-pointer"
                 onClick={() => window.open('/examples')}
