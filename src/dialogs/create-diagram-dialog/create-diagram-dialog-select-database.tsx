d0f2649e6c48af257717a0fc5705d4b75fd33ce1
Guy Ben-Aharon <baguy3@gmail.com>
2024-09-01 16:43:08 +0300

    create diagram dialog refactor

---
+++ src/dialogs/create-diagram-dialog/create-diagram-dialog-select-database.tsx
@@ -0,0 +1,155 @@
+import React, { useCallback } from 'react';
+import { Button } from '@/components/button/button';
+import {
+    DialogClose,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+} from '@/components/dialog/dialog';
+
+import { ToggleGroup, ToggleGroupItem } from '@/components/toggle/toggle-group';
+import { DatabaseType } from '@/lib/domain/database-type';
+import { databaseLogoMap } from '@/lib/databases';
+import { Link } from '@/components/link/link';
+import { LayoutGrid } from 'lucide-react';
+import { CreateDiagramDialogStep } from './create-diagram-dialog-step';
+
+export interface CreateDiagramDialogSelectDatabaseProps {
+    setStep: React.Dispatch<React.SetStateAction<CreateDiagramDialogStep>>;
+    databaseType: DatabaseType;
+    setDatabaseType: React.Dispatch<React.SetStateAction<DatabaseType>>;
+    hasExistingDiagram: boolean;
+    createNewDiagram: () => void;
+}
+
+export const CreateDiagramDialogSelectDatabase: React.FC<
+    CreateDiagramDialogSelectDatabaseProps
+> = ({
+    setStep,
+    databaseType,
+    setDatabaseType,
+    hasExistingDiagram,
+    createNewDiagram,
+}) => {
+    const renderDatabaseOption = useCallback((type: DatabaseType) => {
+        const logo = databaseLogoMap[type];
+        return (
+            <ToggleGroupItem
+                value={type}
+                aria-label="Toggle bold"
+                className="flex size-20 md:size-32"
+            >
+                <img src={logo} alt="PostgreSQL" />
+            </ToggleGroupItem>
+        );
+    }, []);
+
+    const renderExamplesOption = useCallback(
+        () => (
+            <Link href="/examples" className="text-primary hover:text-primary">
+                <div className="flex size-20 cursor-pointer flex-col items-center rounded-md border py-3 text-center md:size-32">
+                    <div className="flex flex-1 items-center">
+                        <LayoutGrid size={34} />
+                    </div>
+                    <div className="flex flex-col-reverse">
+                        <div className="hidden text-sm text-primary md:flex">
+                            Check Examples
+                        </div>
+                        <div className="flex text-xs text-primary md:hidden">
+                            Examples
+                        </div>
+                    </div>
+                </div>
+            </Link>
+        ),
+        []
+    );
+
+    const renderHeader = useCallback(() => {
+        return (
+            <DialogHeader>
+                <DialogTitle>What is your Database?</DialogTitle>
+                <DialogDescription>
+                    Each database has its own unique features and capabilities.
+                </DialogDescription>
+            </DialogHeader>
+        );
+    }, []);
+
+    const renderContent = useCallback(() => {
+        return (
+            <div className="flex flex-1 items-center justify-center">
+                <ToggleGroup
+                    value={databaseType}
+                    onValueChange={(value: DatabaseType) => {
+                        if (!value) {
+                            setDatabaseType(DatabaseType.GENERIC);
+                        } else {
+                            setDatabaseType(value);
+                            setStep(CreateDiagramDialogStep.IMPORT_DATABASE);
+                        }
+                    }}
+                    type="single"
+                    className="grid grid-flow-row grid-cols-3 gap-6"
+                >
+                    {renderDatabaseOption(DatabaseType.MYSQL)}
+                    {renderDatabaseOption(DatabaseType.POSTGRESQL)}
+                    {renderDatabaseOption(DatabaseType.MARIADB)}
+                    {renderDatabaseOption(DatabaseType.SQLITE)}
+                    {renderDatabaseOption(DatabaseType.SQL_SERVER)}
+                    {renderExamplesOption()}
+                </ToggleGroup>
+            </div>
+        );
+    }, [
+        databaseType,
+        renderDatabaseOption,
+        renderExamplesOption,
+        setDatabaseType,
+        setStep,
+    ]);
+
+    const renderFooter = useCallback(() => {
+        return (
+            <DialogFooter className="mt-4 flex !justify-between gap-2">
+                {hasExistingDiagram ? (
+                    <DialogClose asChild>
+                        <Button type="button" variant="secondary">
+                            Cancel
+                        </Button>
+                    </DialogClose>
+                ) : (
+                    <div></div>
+                )}
+                <div className="flex flex-col-reverse gap-2 sm:flex-row sm:justify-end sm:space-x-2">
+                    <Button
+                        type="button"
+                        variant="outline"
+                        onClick={createNewDiagram}
+                    >
+                        Empty diagram
+                    </Button>
+                    <Button
+                        type="button"
+                        variant="default"
+                        disabled={databaseType === DatabaseType.GENERIC}
+                        onClick={() =>
+                            setStep(CreateDiagramDialogStep.IMPORT_DATABASE)
+                        }
+                    >
+                        Continue
+                    </Button>
+                </div>
+            </DialogFooter>
+        );
+    }, [createNewDiagram, databaseType, hasExistingDiagram, setStep]);
+
+    return (
+        <>
+            {renderHeader()}
+            {renderContent()}
+            {renderFooter()}
+        </>
+    );
+};
