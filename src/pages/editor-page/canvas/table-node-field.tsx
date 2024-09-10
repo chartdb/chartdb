c6cc432b92cd31a2ce603f8a45843a0188445b93
johnnyfish <jonathanfishner11@gmail.com>
2024-09-10 17:33:31 +0300

    tmp add expand

--- src/pages/editor-page/canvas/table-node-field.tsx
+++ src/pages/editor-page/canvas/table-node-field.tsx
@@ -22,97 +22,95 @@ export interface TableNodeFieldProps {
     highlighted: boolean;
 }
 
-export const TableNodeField: React.FC<TableNodeFieldProps> = ({
-    field,
-    focused,
-    tableNodeId,
-    highlighted,
-}) => {
-    const { removeField, relationships } = useChartDB();
-    const updateNodeInternals = useUpdateNodeInternals();
-    const connection = useConnection();
-    const isTarget =
-        connection.inProgress && connection.fromNode.id !== tableNodeId;
-    const numberOfEdgesToField = relationships.filter(
-        (relationship) =>
-            relationship.targetTableId === tableNodeId &&
-            relationship.targetFieldId === field.id
-    ).length;
+export const TableNodeField: React.FC<TableNodeFieldProps> = React.memo(
+    ({ field, focused, tableNodeId, highlighted }) => {
+        const { removeField, relationships } = useChartDB();
+        const updateNodeInternals = useUpdateNodeInternals();
+        const connection = useConnection();
+        const isTarget =
+            connection.inProgress && connection.fromNode.id !== tableNodeId;
+        const numberOfEdgesToField = relationships.filter(
+            (relationship) =>
+                relationship.targetTableId === tableNodeId &&
+                relationship.targetFieldId === field.id
+        ).length;
 
-    useEffect(() => {
-        updateNodeInternals(tableNodeId);
-    }, [tableNodeId, updateNodeInternals, numberOfEdgesToField]);
+        useEffect(() => {
+            updateNodeInternals(tableNodeId);
+        }, [tableNodeId, updateNodeInternals, numberOfEdgesToField]);
 
-    return (
-        <div
-            className={`group relative flex h-8 items-center justify-between gap-1 border-t px-3 text-sm last:rounded-b-[6px] hover:bg-slate-100 dark:hover:bg-slate-800 ${highlighted ? 'bg-pink-100 dark:bg-pink-900' : ''} transition-colors`}
-        >
-            {!connection.inProgress && (
-                <>
-                    <Handle
-                        id={`${RIGHT_HANDLE_ID_PREFIX}${field.id}`}
-                        className={`!h-4 !w-4 !border-2 !bg-pink-600 ${!focused ? '!invisible' : ''}`}
-                        position={Position.Right}
-                        type="source"
-                    />
-                    <Handle
-                        id={`${LEFT_HANDLE_ID_PREFIX}${field.id}`}
-                        className={`!h-4 !w-4 !border-2 !bg-pink-600 ${!focused ? '!invisible' : ''}`}
-                        position={Position.Left}
-                        type="source"
-                    />
-                </>
-            )}
-            {(!connection.inProgress || isTarget) && (
-                <>
-                    {Array.from(
-                        { length: numberOfEdgesToField },
-                        (_, index) => index
-                    ).map((index) => (
+        return (
+            <div
+                className={`group relative flex h-8 items-center justify-between gap-1 border-t px-3 text-sm last:rounded-b-[6px] hover:bg-slate-100 dark:hover:bg-slate-800 ${highlighted ? 'bg-pink-100 dark:bg-pink-900' : ''} transition-colors`}
+            >
+                {!connection.inProgress && (
+                    <>
                         <Handle
-                            id={`${TARGET_ID_PREFIX}${index}_${field.id}`}
-                            key={`${TARGET_ID_PREFIX}${index}_${field.id}`}
-                            className={`!invisible`}
+                            id={`${RIGHT_HANDLE_ID_PREFIX}${field.id}`}
+                            className={`!h-4 !w-4 !border-2 !bg-pink-600 ${!focused ? '!invisible' : ''}`}
+                            position={Position.Right}
+                            type="source"
+                        />
+                        <Handle
+                            id={`${LEFT_HANDLE_ID_PREFIX}${field.id}`}
+                            className={`!h-4 !w-4 !border-2 !bg-pink-600 ${!focused ? '!invisible' : ''}`}
+                            position={Position.Left}
+                            type="source"
+                        />
+                    </>
+                )}
+                {(!connection.inProgress || isTarget) && (
+                    <>
+                        {Array.from(
+                            { length: numberOfEdgesToField },
+                            (_, index) => index
+                        ).map((index) => (
+                            <Handle
+                                id={`${TARGET_ID_PREFIX}${index}_${field.id}`}
+                                key={`${TARGET_ID_PREFIX}${index}_${field.id}`}
+                                className={`!invisible`}
+                                position={Position.Left}
+                                type="target"
+                            />
+                        ))}
+                        <Handle
+                            id={`${TARGET_ID_PREFIX}${numberOfEdgesToField}_${field.id}`}
+                            className={
+                                isTarget
+                                    ? '!absolute !left-0 !top-0 !h-full !w-full !transform-none !rounded-none !border-none !opacity-0'
+                                    : `!invisible`
+                            }
                             position={Position.Left}
                             type="target"
                         />
-                    ))}
-                    <Handle
-                        id={`${TARGET_ID_PREFIX}${numberOfEdgesToField}_${field.id}`}
-                        className={
-                            isTarget
-                                ? '!absolute !left-0 !top-0 !h-full !w-full !transform-none !rounded-none !border-none !opacity-0'
-                                : `!invisible`
-                        }
-                        position={Position.Left}
-                        type="target"
-                    />
-                </>
-            )}
-            <div className="block truncate text-left">{field.name}</div>
-            <div className="flex max-w-[35%] justify-end gap-2 truncate hover:shrink-0">
-                {field.primaryKey ? (
-                    <div className="text-muted-foreground group-hover:hidden">
-                        <KeyRound size={14} />
-                    </div>
-                ) : null}
+                    </>
+                )}
+                <div className="block truncate text-left">{field.name}</div>
+                <div className="flex max-w-[35%] justify-end gap-2 truncate hover:shrink-0">
+                    {field.primaryKey ? (
+                        <div className="text-muted-foreground group-hover:hidden">
+                            <KeyRound size={14} />
+                        </div>
+                    ) : null}
 
-                <div className="content-center truncate text-right text-xs text-muted-foreground group-hover:hidden">
-                    {field.type.name}
-                </div>
-                <div className="hidden flex-row group-hover:flex">
-                    <Button
-                        variant="ghost"
-                        className="size-6 p-0 hover:bg-primary-foreground"
-                        onClick={(e) => {
-                            e.stopPropagation();
-                            removeField(tableNodeId, field.id);
-                        }}
-                    >
-                        <Trash2 className="size-3.5 text-red-700" />
-                    </Button>
+                    <div className="content-center truncate text-right text-xs text-muted-foreground group-hover:hidden">
+                        {field.type.name}
+                    </div>
+                    <div className="hidden flex-row group-hover:flex">
+                        <Button
+                            variant="ghost"
+                            className="size-6 p-0 hover:bg-primary-foreground"
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                removeField(tableNodeId, field.id);
+                            }}
+                        >
+                            <Trash2 className="size-3.5 text-red-700" />
+                        </Button>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+);
+TableNodeField.displayName = 'TableNodeField';
