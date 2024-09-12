886362876a4e12855ffef034583dec2b3f94a51b
Guy Ben-Aharon <baguy3@gmail.com>
2024-09-12 19:08:10 +0300

    default hide cardinality

--- src/context/local-config-context/local-config-provider.tsx
+++ src/context/local-config-context/local-config-provider.tsx
@@ -29,7 +29,7 @@ export const LocalConfigProvider: React.FC<React.PropsWithChildren> = ({
     );
 
     const [showCardinality, setShowCardinality] = React.useState<boolean>(
-        (localStorage.getItem(showCardinalityKey) || 'true') === 'true'
+        (localStorage.getItem(showCardinalityKey) || 'false') === 'true'
     );
 
     useEffect(() => {
