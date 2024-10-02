29d95d104c424b7fce790f2fc2a7d775d7786e59
Guy Ben-Aharon <baguy3@gmail.com>
2024-10-02 17:54:23 +0300

    add find overlapping functionality

--- src/pages/editor-page/canvas/canvas-utils.ts
+++ src/pages/editor-page/canvas/canvas-utils.ts
@@ -1,4 +1,5 @@
 import type { Cardinality } from '@/lib/domain/db-relationship';
+import type { TableNodeType } from './table-node/table-node';
 
 export const getCardinalityMarkerId = ({
     cardinality,
@@ -10,3 +11,52 @@ export const getCardinalityMarkerId = ({
     side: 'left' | 'right';
 }) =>
     `cardinality_${selected ? 'selected' : 'not_selected'}_${cardinality}_${side}`;
+
+export const findTableOverlapping = (
+    table: TableNodeType,
+    tables: TableNodeType[]
+): string[] => {
+    const overlappingTables = new Set<string>();
+    const tableRect = {
+        id: table.id,
+        left: table.position.x,
+        right: table.position.x + (table.measured?.width ?? 0),
+        top: table.position.y,
+        bottom: table.position.y + (table.measured?.height ?? 0),
+    };
+
+    for (const otherTable of tables) {
+        if (table.id === otherTable.id) {
+            continue;
+        }
+
+        const otherTableRect = {
+            id: otherTable.id,
+            left: otherTable.position.x,
+            right: otherTable.position.x + (otherTable.measured?.width ?? 0),
+            top: otherTable.position.y,
+            bottom: otherTable.position.y + (otherTable.measured?.height ?? 0),
+        };
+
+        if (
+            tableRect.left < otherTableRect.right &&
+            tableRect.right > otherTableRect.left &&
+            tableRect.top < otherTableRect.bottom &&
+            tableRect.bottom > otherTableRect.top
+        ) {
+            overlappingTables.add(otherTableRect.id);
+        }
+    }
+
+    return Array.from(overlappingTables);
+};
+
+export const findOverlappingTables = (tables: TableNodeType[]): string[] => {
+    const overlappingTables: string[] = [];
+    for (const table of tables) {
+        const currentOverlappingTables = findTableOverlapping(table, tables);
+        overlappingTables.push(...currentOverlappingTables);
+    }
+
+    return Array.from(new Set(overlappingTables));
+};
