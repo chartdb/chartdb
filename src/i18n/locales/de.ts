d086c39c91ae52f6283bc753d8e84ab4a1219238
johnnyfish <jonathanfishner11@gmail.com>
2024-10-19 10:43:23 +0300

    fix build

--- src/i18n/locales/de.ts
+++ src/i18n/locales/de.ts
@@ -226,6 +226,7 @@ export const de: LanguageTranslation = {
                 },
                 // TODO: Translate
                 instructions_link: 'Need help? Watch how',
+                check_json: 'Check JSON',
             },
 
             cancel: 'Abbrechen',
