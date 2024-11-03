fe96d41d1302106088e7d3d22609b759c352af23
Guy Ben-Aharon <baguy3@gmail.com>
2024-11-03 14:30:58 +0200

    wrap helmet (#303)

--- src/helmet/helmet-data.tsx
+++ src/helmet/helmet-data.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Helmet } from 'react-helmet-async';
+import { Helmet } from './helmet-wrapper';
 
 export const HelmetData: React.FC = () => (
     <Helmet>
---
+++ src/helmet/helmet-wrapper.tsx
@@ -0,0 +1,3 @@
+import * as ReactHelmetAsync from 'react-helmet-async';
+
+export const { Helmet, HelmetProvider } = ReactHelmetAsync;
