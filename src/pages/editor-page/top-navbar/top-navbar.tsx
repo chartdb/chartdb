20b2ae436cc18fb73e40d660a1f170068e52284d
Помаранча <105906625+QmanaQ@users.noreply.github.com>
2024-11-06 10:20:14 +0200

    Add uk language (#338)
    * Create uk.ts
    
    added Ukrainian language. I don't know what kind of service it is, but I just helped with the translation into my native language
    
    * Update uk.ts
    
    now all untranslated item (2) is translated
    
    * fix build
    
    * add language to menu
    
    ---------
    
    Co-authored-by: Guy Ben-Aharon <guybenah@gmail.com>

--- src/pages/editor-page/top-navbar/top-navbar.tsx
+++ src/pages/editor-page/top-navbar/top-navbar.tsx
@@ -40,6 +40,7 @@ import { hiMetadata } from '@/i18n/locales/hi';
 import { DiagramName } from './diagram-name';
 import { LastSaved } from './last-saved';
 import { pt_BRMetadata } from '@/i18n/locales/pt_BR';
+import { ukMetadata } from '@/i18n/locales/uk';
 
 export interface TopNavbarProps {}
 
@@ -643,6 +644,17 @@ export const TopNavbar: React.FC<TopNavbarProps> = () => {
                                             }
                                         >
                                             {pt_BRMetadata.name}
+                                        </MenubarCheckboxItem>
+                                        <MenubarCheckboxItem
+                                            onClick={() =>
+                                                changeLanguage(ukMetadata.code)
+                                            }
+                                            checked={
+                                                i18n.language ===
+                                                ukMetadata.code
+                                            }
+                                        >
+                                            {ukMetadata.name}
                                         </MenubarCheckboxItem>
                                     </MenubarSubContent>
                                 </MenubarSub>
