1ee87a2f72c2fa32963f47133360521dee88e862
johnnyfish <jonathanfishner11@gmail.com>
2024-11-06 23:34:58 +0200

    fix(import json): for Check Script Result, default with quotes

--- src/lib/data/import-metadata/utils.ts
+++ src/lib/data/import-metadata/utils.ts
@@ -5,17 +5,24 @@ export const fixMetadataJson = async (
     metadataJson: string
 ): Promise<string> => {
     await waitFor(1000);
-    return metadataJson
-        .trim()
-        .replace(/^[^{]*/, '') // Remove everything before the first '{'
-        .replace(/}[^}]*$/, '}') // Remove everything after the last '}'
-        .replace(/^\s+|\s+$/g, '')
-        .replace(/^"|"$/g, '')
-        .replace(/^'|'$/g, '')
-        .replace(/(?<=:\s*)""(?=\s*[,}])/g, '___EMPTY___') // Temporarily replace empty strings
-        .replace(/""/g, '"') // Replace remaining double quotes
-        .replace(/___EMPTY___/g, '""') // Restore empty strings
-        .replace(/\n/g, '');
+
+    // TODO: remove this temporary eslint disable
+    return (
+        metadataJson
+            .trim()
+            .replace(/^[^{]*/, '') // Remove everything before the first '{'
+            .replace(/}[^}]*$/, '}') // Remove everything after the last '}'
+            .replace(/^\s+|\s+$/g, '')
+            .replace(/^"|"$/g, '')
+            .replace(/^'|'$/g, '')
+            /* eslint-disable-next-line no-useless-escape */
+            .replace(/\"/g, '___ESCAPED_QUOTE___') // Temporarily replace empty strings
+            .replace(/(?<=:\s*)""(?=\s*[,}])/g, '___EMPTY___') // Temporarily replace empty strings
+            .replace(/""/g, '"') // Replace remaining double quotes
+            .replace(/___ESCAPED_QUOTE___/g, '"') // Restore empty strings
+            .replace(/___EMPTY___/g, '""') // Restore empty strings
+            .replace(/\n/g, '')
+    );
 };
 
 export const isStringMetadataJson = (metadataJsonString: string): boolean => {
