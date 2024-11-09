28296d637397cb08f36f37cc35b2715da363f382
Guy Ben-Aharon <baguy3@gmail.com>
2024-11-09 03:35:47 +0200

    fix tags

--- src/templates-data/templates/visual-novel-db.ts
+++ src/templates-data/templates/visual-novel-db.ts
@@ -11,7 +11,7 @@ export const visualNovelDb: Template = {
         'A comprehensive database for information about visual novels.',
     image,
     imageDark,
-    tags: ['Postgres', 'The Visual Novel Database'],
+    tags: ['Postgres'],
     featured: true,
     url: 'https://vndb.org',
     diagram: {
