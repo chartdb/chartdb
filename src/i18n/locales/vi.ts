135d52f3613b68928adc6162f6cd0d8a22164e7c
Guy Ben-Aharon <baguy3@gmail.com>
2024-11-25 14:39:09 +0200

    fix(i18n): add missing type to vi.ts

--- src/i18n/locales/vi.ts
+++ src/i18n/locales/vi.ts
@@ -1,6 +1,6 @@
-import type { LanguageMetadata } from '../types';
+import type { LanguageMetadata, LanguageTranslation } from '../types';
 
-export const vi = {
+export const vi: LanguageTranslation = {
     translation: {
         menu: {
             file: {
