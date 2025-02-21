bb0a55a6913babac3b611ee2f83cbe057310dc54
Calum <calumsiemer@yahoo.com>
2025-02-20 20:52:15 -0500

    no longer needed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# Check if diagram file exists in mounted volume
-if [ -f "/diagram/diagram.json" ]; then
-    # Copy the diagram file to the nginx html directory
-    cp /diagram/diagram.json /usr/share/nginx/html/diagram.json
-fi
 
 # Replace placeholders in nginx.conf
 envsubst '${OPENAI_API_KEY} ${OPENAI_API_ENDPOINT} ${LLM_MODEL_NAME} ${HIDE_BUCKLE_DOT_DEV}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
