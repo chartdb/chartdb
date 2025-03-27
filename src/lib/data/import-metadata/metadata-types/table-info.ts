ba78ccb22804ea2b00f8bed8d9fe29cebdd9bffe
johnnyfish <jonathanfishner11@gmail.com>
2025-03-27 17:23:05 +0200

    fix mssql

--- src/lib/data/import-metadata/metadata-types/table-info.ts
+++ src/lib/data/import-metadata/metadata-types/table-info.ts
@@ -3,19 +3,19 @@ import { z } from 'zod';
 export interface TableInfo {
     schema: string;
     table: string;
-    rows: number;
-    type: string;
-    engine: string;
-    collation: string;
+    rows?: number;
+    type?: string;
+    engine?: string;
+    collation?: string;
     comment?: string;
 }
 
 export const TableInfoSchema: z.ZodType<TableInfo> = z.object({
     schema: z.string(),
     table: z.string(),
-    rows: z.number(),
-    type: z.string(),
-    engine: z.string(),
-    collation: z.string(),
+    rows: z.number().optional(),
+    type: z.string().optional(),
+    engine: z.string().optional(),
+    collation: z.string().optional(),
     comment: z.string().optional(),
 });
