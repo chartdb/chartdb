55216127a219fa300273ac4b4d93f690c2d4de78
Guy Ben-Aharon <baguy3@gmail.com>
2025-04-13 16:19:02 +0300

    fix(sidebar): turn sidebar to responsive for mobile

--- src/pages/editor-page/top-navbar/top-navbar-mock.tsx
+++ src/pages/editor-page/top-navbar/top-navbar-mock.tsx
@@ -1,30 +1,13 @@
-import React, { useCallback } from 'react';
+import React from 'react';
+
 import ChartDBLogo from '@/assets/logo-light.png';
 import ChartDBDarkLogo from '@/assets/logo-dark.png';
 import { useTheme } from '@/hooks/use-theme';
-import { DiagramName } from './diagram-name';
-import { LastSaved } from './last-saved';
-import { LanguageNav } from './language-nav/language-nav';
-import { Menu } from './menu/menu';
 
-export interface TopNavbarProps {}
-
-export const TopNavbar: React.FC<TopNavbarProps> = () => {
+export const TopNavbarMock: React.FC = () => {
     const { effectiveTheme } = useTheme();
-
-    const renderStars = useCallback(() => {
-        return (
-            <iframe
-                src={`https://ghbtns.com/github-btn.html?user=chartdb&repo=chartdb&type=star&size=large&text=false`}
-                width="40"
-                height="30"
-                title="GitHub"
-            ></iframe>
-        );
-    }, []);
-
     return (
-        <nav className="flex flex-col justify-between border-b px-3 md:h-12 md:flex-row md:items-center md:px-4">
+        <nav className="flex h-[105px] flex-col justify-between border-b px-3 md:h-12 md:flex-row md:items-center md:px-4">
             <div className="flex flex-1 flex-col justify-between gap-x-1 md:flex-row md:justify-normal">
                 <div className="flex items-center justify-between pt-[8px] font-primary md:py-[10px]">
                     <a
@@ -43,13 +26,6 @@ export const TopNavbar: React.FC<TopNavbarProps> = () => {
                         />
                     </a>
                 </div>
-                <Menu />
-            </div>
-            <DiagramName />
-            <div className="hidden flex-1 items-center justify-end gap-2 sm:flex">
-                <LastSaved />
-                {renderStars()}
-                <LanguageNav />
             </div>
         </nav>
     );
