d9d0d62dd1f19daf435eb38bdd2e19f03af1ed13
Guy Ben-Aharon <baguy3@gmail.com>
2025-05-08 14:26:27 +0300

    fix build

--- src/i18n/locales/tr.ts
+++ src/i18n/locales/tr.ts
@@ -346,6 +346,12 @@ export const tr: LanguageTranslation = {
             scale_4x: '4x',
             cancel: 'İptal',
             export: 'Dışa Aktar',
+            // TODO: Translate
+            advanced_options: 'Advanced Options',
+            pattern: 'Include background pattern',
+            pattern_description: 'Add subtle grid pattern to background.',
+            transparent: 'Transparent background',
+            transparent_description: 'Remove background color from image.',
         },
         new_table_schema_dialog: {
             title: 'Şema Seç',
