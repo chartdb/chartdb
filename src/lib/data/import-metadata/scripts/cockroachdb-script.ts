bebd66e1fc113f8ba4147aa11d127cd0bfeeb843
johnnyfish <jonathanfishner11@gmail.com>
2025-05-14 18:52:11 +0300

    fix(view_definition): remove view_definition in cockroachdb & maria

--- src/lib/data/import-metadata/scripts/cockroachdb-script.ts
+++ src/lib/data/import-metadata/scripts/cockroachdb-script.ts
@@ -176,8 +176,7 @@ cols AS (
 ), views AS (
     SELECT array_to_string(array_agg(CONCAT('{"schema":"', views.schemaname::TEXT,
                       '","view_name":"', viewname::TEXT,
-                      '","view_definition":"', encode(convert_to(REPLACE(definition::TEXT, '"', '\\"'), 'UTF8'), 'base64'),
-                    '"}')),
+                      '","view_definition":""}')),
                       ',') AS views_metadata
     FROM pg_views views
     WHERE views.schemaname NOT IN ('information_schema', 'pg_catalog')${cockroachdbViewsFilter}
