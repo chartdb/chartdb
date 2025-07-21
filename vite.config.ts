3dbb64aa89c18c7465d99fef9fcb7e1570bbfca6
Guy Ben-Aharon <baguy3@gmail.com>
2025-07-21 11:27:37 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,6 @@ import UnpluginInjectPreload from 'unplugin-inject-preload/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    optimizeDeps: {
-        exclude: [],
-    },
     plugins: [
         react(),
         visualizer({
