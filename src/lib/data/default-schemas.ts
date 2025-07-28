dba372d25a8c642baf8600d05aa154882729d446
Guy Ben-Aharon <baguy3@gmail.com>
2025-07-28 18:55:05 +0300

    fix(cockroachdb): support schema creation for cockroachdb (#803)

--- src/lib/data/default-schemas.ts
+++ src/lib/data/default-schemas.ts
@@ -4,4 +4,5 @@ export const defaultSchemas: { [key in DatabaseType]?: string } = {
     [DatabaseType.POSTGRESQL]: 'public',
     [DatabaseType.SQL_SERVER]: 'dbo',
     [DatabaseType.CLICKHOUSE]: 'default',
+    [DatabaseType.COCKROACHDB]: 'public',
 };
