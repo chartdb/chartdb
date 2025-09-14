fadb4401ae7be2228d64efa2e52526d6e797d297
Guy Ben-Aharon <baguy3@gmail.com>
2025-09-14 20:35:28 +0300

    fix

--- src/pages/editor-page/canvas/table-node/table-edit-mode/table-edit-mode-field.tsx
+++ src/pages/editor-page/canvas/table-node/table-edit-mode/table-edit-mode-field.tsx
@@ -10,31 +10,15 @@ import {
     TooltipTrigger,
 } from '@/components/tooltip/tooltip';
 import { useTranslation } from 'react-i18next';
-import { Toggle } from '@/components/toggle/toggle';
 import { SelectBox } from '@/components/select-box/select-box';
 import { cn } from '@/lib/utils';
+import { TableFieldToggle } from './table-field-toggle';
 
 export interface TableEditModeFieldProps {
     table: DBTable;
     field: DBField;
     focused?: boolean;
 }
-
-const TableFieldToggle = React.forwardRef<
-    React.ElementRef<typeof Toggle>,
-    React.ComponentPropsWithoutRef<typeof Toggle>
->((props, ref) => {
-    return (
-        <Toggle
-            {...props}
-            ref={ref}
-            variant="default"
-            className="h-8 w-[32px] p-2 text-xs text-slate-500 hover:bg-primary-foreground hover:text-slate-700 dark:text-slate-400 dark:hover:text-slate-200"
-        />
-    );
-});
-
-TableFieldToggle.displayName = Toggle.displayName;
 
 export const TableEditModeField: React.FC<TableEditModeFieldProps> = React.memo(
     ({ table, field, focused = false }) => {
